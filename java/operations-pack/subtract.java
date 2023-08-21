ffa854ebebf915728f6d73635cf467b615539ede
FEB <72290574+femyelizabeth@users.noreply.github.com>
2023-08-21 11:42:42 +0530

    Create subtract.java

---
+++ java/operations-pack/subtract.java
@@ -0,0 +1,12 @@
+package operations;
+
+
+public class Substract implements Properties{
+
+    public void calc(int a,int b){
+
+        System.out.println(" Difference is ="+(a-b));
+    }
+
+    
+}
